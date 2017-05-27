f66fd40ee0c87aef5ae3296b2e6a5022ebcb99b1
Geeku <ggongpei@qq.com>
2017-05-27 15:29:35 +0800

    add koa2

---
+++ index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by geeku on 27/05/2017.
+ */
+require('babel-core/register');
+// require the rest of the app that needs to be transpiled after the hook
+const app = require('./src');
