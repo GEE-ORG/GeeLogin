df37c150ac6789e20707fc5c42e8ead0fed9df66
Geeku <ggongpei@qq.com>
2017-06-01 18:02:51 +0800

    add vuex

---
+++ frontend/src/components/Profile.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="profile">
+    <img :src="avatar" alt="avatar">
+    <p class="username">{{ username }}</p>
+    <a href="/signout" class="signout">Sign out</a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'profile',
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
+const store = new Vuex.Store({
+  state: {
+    isLoading: true
+  },
+  mutation: {
+
+  }
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
